f94a3ca59e6c6a1b8cb34aee8bc745ced145f700
Kiran N <ujjwal.nasra@gmail.com>
2022-12-15 14:05:56 +0530

    Handled CORS errors

--- app.js
+++ app.js
@@ -4,10 +4,30 @@ const morgan =  require("morgan");
 const app = express();
 const prroute = require("./api/routes/products");
 const orroute = require("./api/routes/orders");
+const bodyParser = require("body-parser");
 
-app.use(morgan("dev"));  
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+//Handling CORS requests 
+app.use((req,res,next) => {
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,Authorization");
+    //Before actual request , we have an options request sent to client
+    if(req.method === "OPTIONS"){
+        res.header("Access-Control-Allow-Methods","PUT,PATCH,POST,GET,DELETE");
+        return res.status(200).json({});
+    }
+    next();
+});
+
+
+  
 app.use("/product" , prroute ) ; 
 app.use("/orders" , orroute ) ; 
+
 
 
 // Error Handling
